799912f0266b55cc692fe8db346f8e77d395d924
wyukawa <wyukawa@gmail.com>
2008-11-21 08:27:36 +0000

    [update] External Strings
    git-svn-id: https://www.seasar.org/svn/sandbox/s2junit4plugin/trunk/components@26 d213dd46-48dc-4081-854c-85a314177717

--- org.seasar.s2junit4plugin/src/org/seasar/s2junit4plugin/preference/DefaultFolderPreference.java
+++ org.seasar.s2junit4plugin/src/org/seasar/s2junit4plugin/preference/DefaultFolderPreference.java
@@ -7,12 +7,14 @@ import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Text;
+import org.seasar.s2junit4plugin.Constants;
+import org.seasar.s2junit4plugin.Messages;
 
 
 public class DefaultFolderPreference extends Composite{
 	
 	private IPreferenceStore store;
-	private Text mainTestPath;
+	private Text testJavaPath;
 	private Text testResourcesPath;
 	
 	public DefaultFolderPreference(IPreferenceStore store, Composite parent) {
@@ -23,26 +25,26 @@ public class DefaultFolderPreference extends Composite{
 		layout.numColumns = 2;
 		setLayout(layout);
 		Label mainTestPathLabel = new Label(this, SWT.NULL);
-		mainTestPathLabel.setText("テストソースフォルダ");
-		mainTestPath = new Text(this, SWT.BORDER);
-		mainTestPath.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
-		mainTestPath.setText(store.getString("MainTestPath"));
+		mainTestPathLabel.setText(Messages.getString("Preference.TestSourceFolder")); //$NON-NLS-1$
+		testJavaPath = new Text(this, SWT.BORDER);
+		testJavaPath.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
+		testJavaPath.setText(store.getString(Constants.PREF_TEST_JAVA_PATH)); //$NON-NLS-1$
 		Label testResourcesPathLabel = new Label(this, SWT.NULL);
-		testResourcesPathLabel.setText("テストリソースフォルダ");
+		testResourcesPathLabel.setText(Messages.getString("Preference.TestResourceFolder")); //$NON-NLS-1$
 		testResourcesPath = new Text(this, SWT.BORDER);
 		testResourcesPath.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
-		testResourcesPath.setText(store.getString("TestResourcesPath"));
+		testResourcesPath.setText(store.getString("TestResourcesPath")); //$NON-NLS-1$
 	}
 	
 
     public void store() {
-    	store.setValue("MainTestPath", mainTestPath.getText());
-    	store.setValue("TestResourcesPath", testResourcesPath.getText());
+    	store.setValue(Constants.PREF_TEST_JAVA_PATH, testJavaPath.getText()); //$NON-NLS-1$
+    	store.setValue(Constants.PREF_TEST_RESOURCES_PATH, testResourcesPath.getText()); //$NON-NLS-1$
     }
     
     public void loadDefault() {
-    	mainTestPath.setText("src/test/java");
-    	testResourcesPath.setText("src/test/resources");
+    	testJavaPath.setText(Constants.PREF_DEFAULT_TEST_JAVA_PATH); //$NON-NLS-1$
+    	testResourcesPath.setText(Constants.PREF_DEFAULT_TEST_RESOURCES_PATH); //$NON-NLS-1$
     }
 
 }

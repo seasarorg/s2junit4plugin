cce29d98751f7f41a20295035b96b9378057fb6c
PPOI <ppoi@tsukuba-bunko.org>
2008-12-04 08:17:42 +0000

    [update] ライセンス対応。ドキュメントコメントの追記と，EPL/CPLのライセンスファイルを追加。
    git-svn-id: https://www.seasar.org/svn/sandbox/s2junit4plugin/trunk@40 d213dd46-48dc-4081-854c-85a314177717

--- components/org.seasar.s2junit4plugin/src/org/seasar/s2junit4plugin/wizard/NewS2JUnit4TestCaseCreationWizard.java
+++ components/org.seasar.s2junit4plugin/src/org/seasar/s2junit4plugin/wizard/NewS2JUnit4TestCaseCreationWizard.java
@@ -1,4 +1,19 @@
-/*******************************************************************************
+/*
+ * Copyright 2008 the Seasar Foundation and the Others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ *
+ *******************************************************************************
  * Copyright (c) 2000, 2006 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
@@ -7,7 +22,8 @@
  *
  * Contributors:
  *     IBM Corporation - initial API and implementation
- *******************************************************************************/
+ *******************************************************************************
+ */
 package org.seasar.s2junit4plugin.wizard;
 
 import org.eclipse.core.resources.IResource;

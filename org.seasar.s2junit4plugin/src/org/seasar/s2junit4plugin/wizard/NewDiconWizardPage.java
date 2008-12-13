2fad9ee1e937d05b896d4aa69a837574aabad9d2
PPOI <ppoi@tsukuba-bunko.org>
2008-12-13 14:50:47 +0000

    [update] 新規Diconファイル作成Wizardで，パッケージ名とファイル名をユーザが変更できるようにした。 see JUNITPLUGIN-2
    git-svn-id: https://www.seasar.org/svn/sandbox/s2junit4plugin/trunk/components@54 d213dd46-48dc-4081-854c-85a314177717

--- org.seasar.s2junit4plugin/src/org/seasar/s2junit4plugin/wizard/NewDiconWizardPage.java
+++ org.seasar.s2junit4plugin/src/org/seasar/s2junit4plugin/wizard/NewDiconWizardPage.java
@@ -53,27 +53,27 @@ import org.seasar.s2junit4plugin.util.WorkbenchUtil;
  */
 public class NewDiconWizardPage extends NewTypeWizardPage implements ModifyListener {
 	
-	private static final String ID_PLUGIN_UI = "org.seasar.s2junit4plugin";
+	private static final String ID_PLUGIN_UI = "org.seasar.s2junit4plugin"; //$NON-NLS-1$
 	
-	public static final String SYSTEM_ID_DICON_20 = "http://www.seasar.org/dtd/components.dtd";
-	public static final String PUBLIC_ID_DICON_20 = "-//SEASAR//DTD S2Container//EN";
-	public static final String DTD_DICON_20 = "/components.dtd";
-	public static final String DTD_DISPLAY_20 = "2.0";
+	public static final String SYSTEM_ID_DICON_20 = "http://www.seasar.org/dtd/components.dtd"; //$NON-NLS-1$
+	public static final String PUBLIC_ID_DICON_20 = "-//SEASAR//DTD S2Container//EN"; //$NON-NLS-1$
+	public static final String DTD_DICON_20 = "/components.dtd"; //$NON-NLS-1$
+	public static final String DTD_DISPLAY_20 = "2.0"; //$NON-NLS-1$
 
-	public static final String SYSTEM_ID_DICON_21 = "http://www.seasar.org/dtd/components21.dtd";
-	public static final String PUBLIC_ID_DICON_21 = "-//SEASAR2.1//DTD S2Container//EN";
-	public static final String DTD_DICON_21 = "/components21.dtd";
-	public static final String DTD_DISPLAY_21 = "2.1";
+	public static final String SYSTEM_ID_DICON_21 = "http://www.seasar.org/dtd/components21.dtd"; //$NON-NLS-1$
+	public static final String PUBLIC_ID_DICON_21 = "-//SEASAR2.1//DTD S2Container//EN"; //$NON-NLS-1$
+	public static final String DTD_DICON_21 = "/components21.dtd"; //$NON-NLS-1$
+	public static final String DTD_DISPLAY_21 = "2.1"; //$NON-NLS-1$
 
-	public static final String SYSTEM_ID_DICON_23 = "http://www.seasar.org/dtd/components23.dtd";
-	public static final String PUBLIC_ID_DICON_23 = "-//SEASAR//DTD S2Container 2.3//EN";
-	public static final String DTD_DICON_23 = "/components23.dtd";
-	public static final String DTD_DISPLAY_23 = "2.3";
+	public static final String SYSTEM_ID_DICON_23 = "http://www.seasar.org/dtd/components23.dtd"; //$NON-NLS-1$
+	public static final String PUBLIC_ID_DICON_23 = "-//SEASAR//DTD S2Container 2.3//EN"; //$NON-NLS-1$
+	public static final String DTD_DICON_23 = "/components23.dtd"; //$NON-NLS-1$
+	public static final String DTD_DISPLAY_23 = "2.3"; //$NON-NLS-1$
 
-	public static final String SYSTEM_ID_DICON_24 = "http://www.seasar.org/dtd/components24.dtd";
-	public static final String PUBLIC_ID_DICON_24 = "-//SEASAR//DTD S2Container 2.4//EN";
-	public static final String DTD_DICON_24 = "/components24.dtd";
-	public static final String DTD_DISPLAY_24 = "2.4";
+	public static final String SYSTEM_ID_DICON_24 = "http://www.seasar.org/dtd/components24.dtd"; //$NON-NLS-1$
+	public static final String PUBLIC_ID_DICON_24 = "-//SEASAR//DTD S2Container 2.4//EN"; //$NON-NLS-1$
+	public static final String DTD_DICON_24 = "/components24.dtd"; //$NON-NLS-1$
+	public static final String DTD_DISPLAY_24 = "2.4"; //$NON-NLS-1$
 
 	/**
 	 * Dicon のパッケージ
@@ -86,6 +86,11 @@ public class NewDiconWizardPage extends NewTypeWizardPage implements ModifyListe
 	 * Dicon ファイル名
 	 */
 	private String diconFileName;
+
+	/**
+	 *　Diconファイル名設定 Text
+	 */
+	private Text diconFileNameText;
 
 	/**
 	 * 文字エンコーディング設定 Combo
@@ -104,13 +109,13 @@ public class NewDiconWizardPage extends NewTypeWizardPage implements ModifyListe
 	 * @param diconFileName Dicon ファイル名
 	 */
 	public NewDiconWizardPage(IPackageFragment packageFragment, IPackageFragmentRoot packageFragmentRoot, String diconFileName) {
-		super(false, "newDiconPage");
+		super(false, "newDiconPage"); //$NON-NLS-1$
 		this.packageFragment = packageFragment;
 		this.packageFragmentRoot = packageFragmentRoot;
 		this.diconFileName = diconFileName;
 
-		setTitle(Messages.getString("dicon.wizard.NewDiconWizardPage.1"));
-		setDescription(Messages.getString("dicon.wizard.NewDiconWizardPage.2"));
+		setTitle(Messages.getString("dicon.wizard.NewDiconWizardPage.1")); //$NON-NLS-1$
+		setDescription(Messages.getString("dicon.wizard.NewDiconWizardPage.2")); //$NON-NLS-1$
 		setPageComplete(true);
 	}
 
@@ -128,23 +133,21 @@ public class NewDiconWizardPage extends NewTypeWizardPage implements ModifyListe
 
 		createContainerControls(composite, nColumns);
 		createPackageControls(composite, nColumns);
-		setPackageFragment(packageFragment, false);
+		setPackageFragment(packageFragment, true);
 		if(packageFragmentRoot != null) {
 			setPackageFragmentRoot(packageFragmentRoot, true);
 		}
 
 		Label nameLabel = new Label(composite, SWT.NONE);
-		nameLabel.setText(Messages.getString("dicon.wizard.NewDiconWizardPage.3"));
-		nameLabel.setEnabled(false);
+		nameLabel.setText(Messages.getString("dicon.wizard.NewDiconWizardPage.3")); //$NON-NLS-1$
 
-		Text nameText = new Text(composite, SWT.SINGLE | SWT.BORDER);
-		nameText.setText(diconFileName);
+		diconFileNameText = new Text(composite, SWT.SINGLE | SWT.BORDER);
+		diconFileNameText.setText(diconFileName);
 		GridData text1LData1 = new GridData();
 		text1LData1.horizontalAlignment = GridData.FILL;
 		text1LData1.grabExcessHorizontalSpace = true;
-		nameText.setLayoutData(text1LData1);
-		nameText.setEditable(false);
-		nameText.setEnabled(false);
+		diconFileNameText.setLayoutData(text1LData1);
+		diconFileNameText.addModifyListener(this);
 
 		Label nullLabel1 = new Label(composite, SWT.NONE);
 		GridData nullLabelLData1 = new GridData();
@@ -153,7 +156,7 @@ public class NewDiconWizardPage extends NewTypeWizardPage implements ModifyListe
 		nullLabel1.setLayoutData(nullLabelLData1);
 
 		Label encodingLabel = new Label(composite, SWT.NONE);
-		encodingLabel.setText(Messages.getString("dicon.wizard.NewDiconWizardPage.4"));
+		encodingLabel.setText(Messages.getString("dicon.wizard.NewDiconWizardPage.4")); //$NON-NLS-1$
 
 		encodingCombo = new Combo(composite, SWT.NONE);
 		encodingCombo.addModifyListener(this);
@@ -166,7 +169,7 @@ public class NewDiconWizardPage extends NewTypeWizardPage implements ModifyListe
 		nullLabel2.setLayoutData(nullLabelLData2);
 
 		Label versionLabel = new Label(composite, SWT.NONE);
-		versionLabel.setText(Messages.getString("dicon.wizard.NewDiconWizardPage.5"));
+		versionLabel.setText(Messages.getString("dicon.wizard.NewDiconWizardPage.5")); //$NON-NLS-1$
 
 		versionCombo = new Combo(composite, SWT.NONE);
 		initVersionCombo(versionCombo);
@@ -181,7 +184,7 @@ public class NewDiconWizardPage extends NewTypeWizardPage implements ModifyListe
 	 * @param combo 文字エンコーディング設定 Combo
 	 */
 	private void initEncodingCombo(Combo combo) {
-		combo.setText("UTF-8");
+		combo.setText("UTF-8"); //$NON-NLS-1$
 		if (combo.getItemCount() == 0) {
 			String[] encodings = WorkbenchUtil.getAllWorkbenchEncodings();
 			for (int i = 0; i < encodings.length; i++) {
@@ -225,23 +228,40 @@ public class NewDiconWizardPage extends NewTypeWizardPage implements ModifyListe
 	}
 
 	/**
+	 * Diconファイル名設定のステータス情報を生成します。
+	 * @return Diconファイル名設定のステータス情報
+	 */
+	private IStatus createDiconFileNameStatus() {
+		int severity = IStatus.OK;
+		String message = ""; //$NON-NLS-1$
+		if (diconFileNameText != null) {
+			String value = diconFileNameText.getText();
+			if (StringUtil.noneValue(value)) {
+				message = Messages.getString("NewDiconWizardPage.dionFileNameEmpty"); //$NON-NLS-1$
+				severity = IStatus.ERROR;
+			}
+		}
+		return createStatus(severity, message);
+	}
+
+	/**
 	 * Encoding 設定のステータス情報を生成します。
 	 * @return Encoding 設定のステータス情報
 	 */
 	private IStatus createEncodingStatus() {
 		int severity = IStatus.ERROR;
-		String message = "";
+		String message = ""; //$NON-NLS-1$
 		if (encodingCombo != null) {
 			String enc = encodingCombo.getText();
 			if (StringUtil.noneValue(enc)) {
-				message = Messages.getString("dicon.wizard.NewDiconWizardPage.7");
+				message = Messages.getString("dicon.wizard.NewDiconWizardPage.7"); //$NON-NLS-1$
 			} else {
 				try {
-					String test = "enc test string";
+					String test = "enc test string"; //$NON-NLS-1$
 					test.getBytes(enc);
 					severity = IStatus.OK;
 				} catch (UnsupportedEncodingException e) {
-					message = Messages.getString("dicon.wizard.NewDiconWizardPage.8");
+					message = Messages.getString("dicon.wizard.NewDiconWizardPage.8"); //$NON-NLS-1$
 				}
 			}
 		}
@@ -252,11 +272,10 @@ public class NewDiconWizardPage extends NewTypeWizardPage implements ModifyListe
 	 * WizardPage のステータスを更新します。
 	 */
 	private void doStatusUpdate() {
-		IStatus encodingStatus = createEncodingStatus();
-		IStatus[] status = new IStatus[] {
-				fContainerStatus, encodingStatus
+		IStatus[] statuses = new IStatus[] {
+				fContainerStatus, createDiconFileNameStatus(), createEncodingStatus()
 		};
-		updateStatus(status);
+		updateStatus(statuses);
 	}
 
 	/**
@@ -327,28 +346,28 @@ public class NewDiconWizardPage extends NewTypeWizardPage implements ModifyListe
 	 * @return 生成に成功した場合 true，失敗した場合 false
 	 */
 	public boolean createDiconFile() {
+		String fileName = diconFileNameText.getText();
 		String enc = encodingCombo.getText();
 		String publicId = getPublicId(versionCombo.getText());
 		String systemId = getSystemId(versionCombo.getText());
-		String dicon = Messages.getString("dicon.wizard.NewDiconWizardPage.10",
+		String dicon = Messages.getString("dicon.wizard.NewDiconWizardPage.10", //$NON-NLS-1$
 				new Object[] {
 					enc
 				})
-				+ Messages.getString("dicon.wizard.NewDiconWizardPage.11",
+				+ Messages.getString("dicon.wizard.NewDiconWizardPage.11", //$NON-NLS-1$
 						new Object[] {
 								publicId, systemId
 						})
-				+ Messages.getString("dicon.wizard.NewDiconWizardPage.12");
+				+ Messages.getString("dicon.wizard.NewDiconWizardPage.12"); //$NON-NLS-1$
 		try {
 			InputStream contents = new ByteArrayInputStream(dicon.getBytes(enc));
 			IPackageFragmentRoot sourceFolder = getPackageFragmentRoot();
-			IPackageFragment packageFragment = sourceFolder.createPackageFragment(this.packageFragment.getElementName(), false, null);
-			IFile file = FileUtil.createFile(packageFragment, diconFileName, contents);
+			IPackageFragment packageFragment = sourceFolder.createPackageFragment(getPackageFragment().getElementName(), false, null);
+			IFile file = FileUtil.createFile(packageFragment, fileName, contents);
 			if (file != null) {
 				file.setCharset(enc, null);
 				IWorkbench workbench = Activator.getDefault().getWorkbench();
 				IDE.openEditor(workbench.getActiveWorkbenchWindow().getActivePage(), file);
-//				WorkbenchUtils.openDiconEditor(file);
 				return true;
 			}
 		} catch (CoreException e) {

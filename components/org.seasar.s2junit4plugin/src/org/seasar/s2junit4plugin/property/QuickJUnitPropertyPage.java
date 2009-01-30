1afc697efdf086b71068ab9fcd8914cc0690fd28
goooooooooosuke <goooooooooosuke@d213dd46-48dc-4081-854c-85a314177717>
2009-01-30 10:20:16 +0000

    S2JUnit4の設定ページをJavaプロジェクトのプロパティにも追加しました。 また、設定ページのレイアウトを少しだけ変更しました。
    git-svn-id: https://www.seasar.org/svn/sandbox/s2junit4plugin/branches/goooooooooosukeSandbox@78 d213dd46-48dc-4081-854c-85a314177717

---
+++ components/org.seasar.s2junit4plugin/src/org/seasar/s2junit4plugin/property/QuickJUnitPropertyPage.java
@@ -0,0 +1,60 @@
+package org.seasar.s2junit4plugin.property;
+
+import org.eclipse.jdt.core.IJavaProject;
+import org.eclipse.jface.dialogs.Dialog;
+import org.eclipse.jface.dialogs.IDialogConstants;
+import org.eclipse.jface.preference.IPreferenceStore;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
+import org.eclipse.swt.widgets.TabFolder;
+import org.eclipse.swt.widgets.TabItem;
+import org.eclipse.ui.dialogs.PropertyPage;
+import org.seasar.s2junit4plugin.Messages;
+import org.seasar.s2junit4plugin.preference.DefaultFolderPreference;
+import org.seasar.s2junit4plugin.preference.NamingRulesPreference;
+import org.seasar.s2junit4plugin.util.PreferenceStoreUtil;
+
+
+public class QuickJUnitPropertyPage extends PropertyPage {
+	
+	private NamingRulesPreference namingRulesPreference;
+	
+	private DefaultFolderPreference defaultFolderPreference;
+	
+	@Override
+	protected Control createContents(Composite parent) {
+		IPreferenceStore store = PreferenceStoreUtil.getPreferenceStoreOfProject(((IJavaProject) getElement()).getProject());
+		TabFolder tabFolder = new TabFolder(parent, SWT.NULL);
+		tabFolder.setLayoutData(new GridData(GridData.FILL_BOTH));
+		
+		TabItem item1 = new TabItem(tabFolder, SWT.NULL);
+		item1.setText(Messages.getString("Preference.tabItem.defaultFolder")); //$NON-NLS-1$
+		
+		defaultFolderPreference = new DefaultFolderPreference(store, tabFolder);
+		item1.setControl(defaultFolderPreference);
+		
+		TabItem item2 = new TabItem(tabFolder, SWT.NULL);
+		item2.setText(Messages.getString("Preference.tabItem.namingRule")); //$NON-NLS-1$
+		int widthHint = convertHorizontalDLUsToPixels(IDialogConstants.BUTTON_WIDTH);
+		namingRulesPreference = new NamingRulesPreference(store, tabFolder, widthHint);
+		Dialog.applyDialogFont(namingRulesPreference);
+		item2.setControl(namingRulesPreference);
+		
+		return parent;
+	}
+	
+	protected void performDefaults() {
+		super.performDefaults();
+		namingRulesPreference.loadDefault();
+		defaultFolderPreference.loadDefault();
+	}
+	
+	public boolean performOk() {
+		namingRulesPreference.store();
+		defaultFolderPreference.store();
+		return true;
+	}
+	
+}
